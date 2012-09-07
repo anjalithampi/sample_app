2b8d8cdd42b6eae707bbf4b0b6d93ccba26b9fcb
Anjali Thampi <anjalithampi1982@gmail.com>
2012-09-07 12:13:10 +0530

    Adding new test mechanisms   * Added Guard   * Added Spork

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -16,7 +16,15 @@ describe "Static pages" do
     end
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text =>"#{base_title} | Home")
+      page.should have_selector('title', :text =>"#{base_title}")
+    end
+    it "should have the base title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text =>"#{base_title}")
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
@@ -29,7 +37,7 @@ describe "Static pages" do
       visit '/static_pages/help'
       page.should have_selector('h1', :text =>"Help")
     end
-    it "should have the title 'Help'" do
+    it "should have the base title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title', :text =>"#{base_title} | Help")
     end
@@ -44,7 +52,7 @@ describe "Static pages" do
       visit '/static_pages/about'
       page.should have_selector('h1', :text =>"About Us")
     end
-    it "should have the title 'Help'" do
+    it "should have the base title 'Help'" do
       visit '/static_pages/about'
       page.should have_selector('title', :text =>"#{base_title} | About Us")
     end
@@ -59,7 +67,7 @@ describe "Static pages" do
       visit '/static_pages/contact'
       page.should have_selector('h1', :text => "Contact Details")
     end
-    it "should have the title 'Contact Details'" do
+    it "should have the base title 'Contact Details'" do
       visit '/static_pages/contact'
       page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact Details")
     end
