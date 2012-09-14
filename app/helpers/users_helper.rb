52b13a4530f52c87e91eb5899d9211dc1884bcd4
Anjali Thampi <anjalithampi1982@gmail.com>
2012-09-14 14:06:27 +0530

    Sign in test cases

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,11 @@
 module UsersHelper
-  def gravatar_for(user)
+    def gravatar_for(user, options = {:size => 40})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    # From http://blog.douglasfshearer.com/post/17546994128/gravatar-for-ruby-and-ruby-on-rails
+    gravatar_url << "?size=#{options[:size]}" if options[:size]
+    gravatar_url << "?rating=#{options[:rating]}" if options[:rating]
+    gravatar_url << "?default=#{options[:default]}" if options[:default]
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -23,6 +23,23 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+      
+      describe "should show error" do
+        before { click_button submit }
+      
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+      end
+      
+      # describe "should insist that name is present" do
+      #         before do
+      #           fill_in "Email", with: "user@example.com"
+      #           fill_in "Password", with: "foobar"
+      #           fill_in "Confirmation", with: "foobar"
+      #         end
+      #         
+      #         it { should have_selector('div.alert.alert-error', text: "The form")}
+      #       end
     end
     
     describe "with valid information" do
@@ -35,6 +52,14 @@ describe "User pages" do
       
       it "should create a user" do
         expect { click_button submit }.to change(User, :count)
+      end
+      
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email("anjali@example.net") }
+        
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
