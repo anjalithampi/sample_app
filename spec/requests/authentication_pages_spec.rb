c6dcdff87e4b626c614095bb031a462d828a8d54
Anjali Thampi <anjalithampi1982@gmail.com>
2012-09-17 10:26:55 +0530

    Added more methods in utilities.rb

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -9,7 +9,8 @@ describe "Authentication Pages" do
     describe "with invalid information" do
       before { click_button "Sign in" }
 
-      it { should have_selector('title', text: 'Sign in') }
+      #it { should have_selector('title', text: 'Sign in') }
+      it { should have_title('Sign in')}
       #it { should have_selector('div.alert.alert-error', text: 'Invalid') }
       it { should have_error_message('Invalid') }
       
@@ -28,7 +29,8 @@ describe "Authentication Pages" do
       #         click_button "Sign in"
       #       end
   
-      it { should have_selector('title', text: user.name) }
+      #it { should have_selector('title', text: user.name) }
+      it { should have_title(user.name) }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -9,7 +9,7 @@ describe "User pages" do
     before { visit signup_path }
     
     it { should have_selector('h1', text: "Sign up") }
-    it { should have_selector('title', text: full_title('Sign up')) }
+    it { should have_title(full_title('Sign up')) }
     
   end
   
@@ -27,31 +27,15 @@ describe "User pages" do
       describe "should show error" do
         before { click_button submit }
       
-        it { should have_selector('title', text: 'Sign up') }
+        it { should have_title('Sign up') }
         it { should have_content('error') }
-        #it { should have_selector('div.alert.alert-error', text: 'Invalid') }
         it { should have_error_message('Invalid') }
       end
-      
-      # describe "should insist that name is present" do
-      #         before do
-      #           fill_in "Email", with: "user@example.com"
-      #           fill_in "Password", with: "foobar"
-      #           fill_in "Confirmation", with: "foobar"
-      #         end
-      #         
-      #         it { should have_selector('div.alert.alert-error', text: "The form")}
-      #       end
     end
     
     describe "with valid information" do
-      before do
-        fill_in "Name", with: "Example User"
-        fill_in "Email", with: "user@example.com"
-        fill_in "Password", with: "foobar"
-        fill_in "Confirmation", with: "foobar"
-      end
-      
+      before { valid_signup }
+
       it "should create a user" do
         expect { click_button submit }.to change(User, :count)
       end
@@ -60,7 +44,7 @@ describe "User pages" do
         before { click_button submit }
         let(:user) { User.find_by_email("user@example.com") }
         
-        it { should have_selector('title', text: full_title(user.name)) }
+        it { should have_title(full_title(user.name)) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
         it { should have_link('Sign out') }
         
@@ -78,7 +62,7 @@ describe "User pages" do
     before { visit user_path(user) }
     
     it { should have_selector('h1', text: user.name) }
-    it { should have_selector('title', text: user.name) }
+    it { should have_title(user.name) }
   end
   
 end
