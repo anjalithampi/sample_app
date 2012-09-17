12a1bf62afc6b27589d32e9725ff05600c77824e
Anjali Thampi <anjalithampi1982@gmail.com>
2012-09-17 09:42:19 +0530

    Finished sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
+      flash[:error] = "Invalid information"
       render "new"
     end
   end
